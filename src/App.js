b8b49c4f3fc79361b772f333eef587c96b46a74d
Mayra-Riccardi <may_ricca5@hotmail.com>
2022-07-05 05:22:35 -0300

    creacion de navbar con logo y secciones

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar';
+
+//en react hay que crear funciones que son los componentes
+//ejemplo con funcion flecha. el llamado de la funcion va entre llaves, los simbolos de mayor y menos tambine tienen que estar porq en react las funcinones no pueden tomar mas de un elemento
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar/>
     </div>
   );
 }
 
 export default App;
+
+//en react se usan llaves para llamar a la funcion
+//Cuando un componente (funcion) necesita parametros (propiedades props) para funcionar, este componente debe presentarse como un objeto
---
+++ src/components/NavBar.js
@@ -0,0 +1,36 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import logo from '../img/logo.png';
+
+const NavBar = () => {
+    return (
+        <nav className="navbar navbar-expand-lg">
+        <div className="container-fluid">
+        <a className="navbar-brand" href="#">
+        <img src={logo} alt="" width="80" height="80" class="d-inline-block align-text-bottom"/>
+        You Need Sushi
+        </a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav">
+        <li className="nav-item">
+          <a className="nav-link" aria-current="page" href="#">Kits</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Arroz</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Algas</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Salsas</a>
+        </li>
+        </ul>
+        </div>
+        </div>
+        </nav>
+    )
+}
+
+export default NavBar;
